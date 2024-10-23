17191c517925eff0d74efa2a415052098926ad46
xushnudbek <xushnudbek756@gmail.com>
2024-10-23 15:07:50 +0500

    Initial commit

---
+++ src/main/java/uz/eastwaysolutions/lms/eastwaylms/EastwayLmsApplication.java
@@ -0,0 +1,13 @@
+package uz.eastwaysolutions.lms.eastwaylms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EastwayLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EastwayLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/eastwaysolutions/lms/eastwaylms/EastwayLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.eastwaysolutions.lms.eastwaylms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EastwayLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

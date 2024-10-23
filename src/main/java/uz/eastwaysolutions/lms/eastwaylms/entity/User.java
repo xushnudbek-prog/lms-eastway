da3a8611ec1073b697533fa0cf218f0ad5de8ea9
xushnudbek <xushnudbek756@gmail.com>
2024-10-23 15:12:45 +0500

    User Entity created

---
+++ src/main/java/uz/eastwaysolutions/lms/eastwaylms/entity/User.java
@@ -0,0 +1,20 @@
+package uz.eastwaysolutions.lms.eastwaylms.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

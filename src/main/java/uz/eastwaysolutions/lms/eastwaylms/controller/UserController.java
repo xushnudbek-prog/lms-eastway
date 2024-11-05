a1c6d7a6eb771f7d048eede09b4792921a5ba233
jaloliddin <jaloliddintemirov@gmail.com>
2024-11-05 13:54:17 +0500

    Fixed Relation Problem

--- src/main/java/uz/eastwaysolutions/lms/eastwaylms/controller/UserController.java
+++ src/main/java/uz/eastwaysolutions/lms/eastwaylms/controller/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.eastwaysolutions.lms.eastwaylms.dto.user.UpdatePasswordRequest;
 import uz.eastwaysolutions.lms.eastwaylms.dto.user.UpdateUserInfoRequest;
+import uz.eastwaysolutions.lms.eastwaylms.entity.Courses;
 import uz.eastwaysolutions.lms.eastwaylms.service.UserServiceForCRUD;
 import uz.eastwaysolutions.lms.eastwaylms.service.user.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -43,8 +46,15 @@ public class UserController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/unEnrollCourse/{courseId}/{userId}")
-    public ResponseEntity<?> unenrollFromCourse(@PathVariable Long userId, @PathVariable Long courseId) {
+    public ResponseEntity<?> unEnrollFromCourse(@PathVariable Long userId, @PathVariable Long courseId) {
         userServiceForCRUD.unEnrollUserFromCourse(userId, courseId);
         return ResponseEntity.ok("User unEnrolled from course successfully");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/courses/{userId}")
+    public ResponseEntity<List<Courses>> getUserCourse(@PathVariable Long userId){
+        List<Courses> userCourses = userServiceForCRUD.getUserCourses(userId);
+        return ResponseEntity.ok(userCourses);
     }
 }
